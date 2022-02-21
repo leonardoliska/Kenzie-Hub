6aa63563579a85fdb4ab5c83e6b13304284566e9
leonardoliska <leo.liska@hotmail.com>
2022-02-21 16:37:45 -0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css"
+import { Switch, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            <Route exact path="/login">
+                <Login />
+            </Route>
+
+            <Route exact path="/register">
+                <Register />
+            </Route>
+        </Switch>
+    )
 }
 
 export default App
